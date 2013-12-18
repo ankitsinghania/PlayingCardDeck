bb97068399d4d8c16404af9612ad42820fd41f87
Ankit Singhania <ankit3singhania@gmail.com>
2013-12-17 19:32:04 -0500

    Initial Commit

---
+++ PlayingCardDeck/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PlayingCardDeck
+//
+//  Created by Ankit Singhania on 12/17/13.
+//  Copyright (c) 2013 Digitans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PlayingCardDeckTests/PlayingCardDeckTests.m
@@ -0,0 +1,34 @@
+//
+//  PlayingCardDeckTests.m
+//  PlayingCardDeckTests
+//
+//  Created by Ankit Singhania on 12/17/13.
+//  Copyright (c) 2013 Digitans. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PlayingCardDeckTests : XCTestCase
+
+@end
+
+@implementation PlayingCardDeckTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
